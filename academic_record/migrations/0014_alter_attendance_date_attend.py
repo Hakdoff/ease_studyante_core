28616eda69efc21e2becb97dbf1d43d3de4bac36
ErikaFuerte <erika@bpoc.co.jp>
2024-11-06 16:38:39 +0800

    test

--- academic_record/migrations/0014_alter_attendance_date_attend.py
+++ academic_record/migrations/0014_alter_attendance_date_attend.py
@@ -2,7 +2,7 @@
 
 import datetime
 from django.db import migrations, models
-from django.utils.timezone import utc
+from django.utils.timezone import timezone
 
 
 class Migration(migrations.Migration):
@@ -15,6 +15,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='attendance',
             name='date_attend',
-            field=models.DateTimeField(default=datetime.datetime(2024, 3, 19, 23, 0, 3, 76823, tzinfo=utc)),
+            field=models.DateTimeField(default=datetime.datetime(2024, 3, 19, 23, 0, 3, 76823, tzinfo=timezone.utc)),
         ),
     ]
--- aes/aes_implementation.py
+++ aes/aes_implementation.py
@@ -2,9 +2,9 @@
 
 from base64 import b64encode, b64decode
 import hashlib
-from Cryptodome.Cipher import AES
+from Crypto.Cipher import AES
 import os
-from Cryptodome.Random import get_random_bytes
+from Crypto.Random import get_random_bytes
 
 
 # pad with spaces at the end of the text
